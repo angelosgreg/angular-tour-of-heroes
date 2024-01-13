704a3ef25b25f3e98ec147f51808f9bc7b0e587a
Angelos Grigorakis <angelosgreg13@gmail.com>
2024-01-13 15:20:30 +0100

    debug hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-heroes',
+  standalone: true,
+  imports: [],
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
+
 }

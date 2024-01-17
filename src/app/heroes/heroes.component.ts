a9fc6b669a5e5109b279c9bd2c1027b3c4d67b5d
Angelos Grigorakis <angelosgreg13@gmail.com>
2024-01-17 13:00:49 +0200

    css

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,9 +3,13 @@ import { Hero } from '../hero';
 import {HEROES} from './mock-heroes';
 
 @Component({
+  standalone: true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  imports: [
+
+  ],
 })
 export class HeroesComponent {
   heroes = HEROES;

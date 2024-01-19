3f3630354a5a94828670cf558ce090bf552c6a62
Angelos Grigorakis <angelosgreg13@gmail.com>
2024-01-19 13:33:01 +0200

    updated components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
-import { Hero } from '../hero';
+import {Component} from '@angular/core';
+import {
+  NgIf,
+  NgFor,
+  UpperCasePipe,
+} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+import {Hero} from '../hero';
 import {HEROES} from './mock-heroes';
 
 @Component({
@@ -8,13 +15,18 @@ import {HEROES} from './mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
   imports: [
-
+    FormsModule,
+    NgIf,
+    NgFor,
+    UpperCasePipe,
   ],
 })
+
 export class HeroesComponent {
   heroes = HEROES;
   selectedHero?: Hero;
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }

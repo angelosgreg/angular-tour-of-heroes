d1fd4550600d1ac326f54fd8a7deafd68f27b7fc
Angelos Grigorakis <angelosgreg13@gmail.com>
2024-01-22 16:06:17 +0200

    message

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,7 +4,7 @@ import { Observable, of } from 'rxjs';
 
 import { Hero } from './hero';
 import { HEROES } from '../mock-heroes';
-import { MessageService } from '../message.service';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root',
